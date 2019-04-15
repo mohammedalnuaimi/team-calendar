ee2a2229a6c4478cdd2f75ab93bb1d343c0aea8e
Mohammed A <closenet.uk@gmail.com>
2019-04-16 00:46:43 +0100

    g Please enter the commit message for your changes. Lines starting

--- src/index.js
+++ src/index.js
@@ -3,121 +3,76 @@ import Radium from 'radium'
 import './index.css';
 import ReactDOM from 'react-dom'
 import Box from './comp/box.js';
+import axios from 'axios';
 
-// class Times extends React.Component {
+class Times extends React.Component {
 
-//     state = {
-//         number: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
-//         boxKey: [],
-//         activeIndex: null
-//     }
-
-
-//     hoveredOver = () => {
-//     }
-
-//     handleClick = (index) => this.setState({ activeIndex: index })
-
-//     clickedBtn= (index) =>  console.log("xxxxx", index)
-//        // this.setState({ activeIndex: index })
-
-
-//     render() {
-//         const styles = {
-//             base: {
-//                 backgroundColor: 'black',
-//                 color: 'yellow',
-//                 ":hover": {
-//                     backgroundColor: "green",
-//                     color: "red"
-//                 },
-//                 ":active": {
-//                     backgroundColor: "green",
-//                     color: "yellow"
-//                 }
-//             }
-//         }
-
-//         let data = null
-//         data = (
-//             <div className='inner-matrix'>
-//                 {
-//                     this.state.number.map((item) => {
-//                         if (item === 8 || item === 15 || item === 22 || item === 29) {
-//                             return <React.Fragment> <br />
-//                                 <Box number={item}
-//                                     style={styles.base}
-//                                     index={item}
-//                                     isActive={this.state.activeIndex === item}
-//                                    // click={this. }
-//                                     hover={this.hoveredOver} />
-//                             </React.Fragment>
-//                         } else {
-//                             return <Box number={item}
-//                                 style={styles.base}
-//                                 index={item}
-//                                 isActive={this.state.activeIndex === item}
-//                                 click={this.clickedBtn  }
-//                                 hover={this.hoveredOver} />
-//                         }
-//                     })
-//                 }
-//             </div>
-//         )
-
-//         return (
-//             <div className='matrix' >
-//                 <h1 className='title'> Log your dates</h1>
-//                 {data}
-//             </div>
-//         )
-//     }
-// }
-
-const STATUS = {
-    HOVERED: 'hovered',
-    NORMAL: 'normal',
-};
-
-export default class Link extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this._onMouseEnter = this._onMouseEnter.bind(this);
-        this._onMouseLeave = this._onMouseLeave.bind(this);
-
-        this.state = {
-            class: STATUS.NORMAL,
-        };
+    state = {
+        number: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
+        boxKey: [],
+        activeIndex: null
     }
 
-    _onMouseEnter() {
-        this.setState({ class: STATUS.HOVERED });
+
+    hoveredOver = () => {
     }
 
-    _onMouseLeave() {
-        this.setState({ class: STATUS.NORMAL });
-    }
+    handleClick = (index) => this.setState({ activeIndex: index })
+
+    clickedBtn= (index) =>  console.log("xxxxx", index)
+       // this.setState({ activeIndex: index })
+
 
     render() {
-        return (
-            
-                <a
-                    className={this.state.class}
-                    href={this.props.page || '#'}
-                    onMouseEnter={this._onMouseEnter}
-                    onMouseLeave={this._onMouseLeave}
-                >
-                    {this.props.children}
-                </a>
+        const styles = {
+            base: {
+                backgroundColor: 'black',
+                color: 'yellow',
+                ":hover": {
+                    backgroundColor: "green",
+                    color: "red"
+                },
+                ":active": {
+                    backgroundColor: "green",
+                    color: "yellow"
+                }
+            }
+        }
 
-        );
+        let data = null
+        data = (
+            <div className='inner-matrix'>
+                {
+                    this.state.number.map((item) => {
+                        if (item === 8 || item === 15 || item === 22 || item === 29) {
+                            return <React.Fragment> <br />
+                                <Box number={item}
+                                    style={styles.base}
+                                    index={item}
+                                    isActive={this.state.activeIndex === item}
+                                   // click={this. }
+                                    hover={this.hoveredOver} />
+                            </React.Fragment>
+                        } else {
+                            return <Box number={item}
+                                style={styles.base}
+                                index={item}
+                                isActive={this.state.activeIndex === item}
+                                click={this.clickedBtn  }
+                                hover={this.hoveredOver} />
+                        }
+                    })
+                }
+            </div>
+        )
+
+        return (
+            <div className='matrix' >
+                <h1 className='title'> Log your dates</h1>
+                {data}
+            </div>
+        )
     }
 }
 
-ReactDOM.render(
-    // <Times />,
-    <Link />,
-    document.getElementById('root')
-);
-// export default Radium(Times);
+ReactDOM.render(<Times />, document.getElementById('root'));
